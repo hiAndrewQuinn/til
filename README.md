87013d14f2cdd90f7c105eb7f98382ce01b4408b
Andrew Quinn <53230903+hiAndrewQuinn@users.noreply.github.com>
2023-11-17 22:05:05 +0200

    feat: Add my lil TIL script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# til
-Today I Learned (TIL)
+# Today I Learned (TIL)
+
+I learn a lot of new things every day. At some point I decided I wanted to have
+a public record of some of them, so I wrote a [shell script](./til.fish) to let
+me open a terminal, type `til`, and immediately jot down whatever little tidbit
+I just came across. This repo is the result of that.
+
+## Quickstart
+
+_In case you want to make a repo like this for yourself:_
+
+- Remove `.git/` and all of my `.md` files (except `README.md`, of course).
+- Install `fish`, `git`, `kitty`, and `nvim`.
+- `cp til.fish ~/.config/fish/functions/` to get the command into your shell.
+- Run `til` at the Fish shell and watch the magic.
---
+++ til.fish
@@ -0,0 +1,40 @@
+#!/bin/fish
+
+
+set til_path ~/Code/til/
+
+function read_title
+    set prompt 'set_color green; echo -n "Title"; set_color normal; echo "> "'
+    read -p $prompt -g title
+end
+
+function to_kebab_case
+    echo $argv | tr '[:upper:]' '[:lower:]' | tr -cs '[:alnum:]' - | sed 's/^-//' | sed 's/-$//'
+end
+
+
+function til
+    read_title
+    set file_name (to_kebab_case $title).md
+    set file_path $til_path/$file_name
+
+    set -l frontmatter "---
+title: $title
+date: $(date)
+tags: 
+---
+
+"
+
+    echo $frontmatter >$file_path
+
+    kitty --title til -e -- nvim $file_path + -c startinsert 2>/dev/null
+
+    pushd $til_path
+
+    git add $file_name
+    git commit -m "new til: $title"
+    git push
+
+    popd
+end
